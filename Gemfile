63088dafce4490b9800912a0df0d33bc3c0e0493
Jevon Mckenzie <jevonmckenzie.wesleycollege@gmail.com>
2019-11-07 12:13:18 -0600

    edited files to include Admin Gem

--- Gemfile
+++ Gemfile
@@ -5,6 +5,10 @@ ruby '2.6.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.0'
+
+gem 'rails_admin'
+
+gem 'skins'
 # Use postgreql as the database for Active Record
 gem 'pg'
 # Use cancan for user authorization
--- app/controllers/supports_controller.rb
+++ app/controllers/supports_controller.rb
@@ -1,6 +1,6 @@
 class SupportsController < ApplicationController
   before_action :set_support, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:show, :index]
+  before_action :authenticate_user!#, except: [:show, :index]
   # GET /supports
   # GET /supports.json
   def index
