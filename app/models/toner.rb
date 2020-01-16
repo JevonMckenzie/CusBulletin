9dc80f20126556de8a4419f011221b3d9d74a849
Jevon Mckenzie <jevonmckenzie.wesleycollege@gmail.com>
2020-01-16 10:00:53 -0600

    edited toner model to remove readonly attributes so Admin can edit

--- app/models/toner.rb
+++ app/models/toner.rb
@@ -1,5 +1,5 @@
 class Toner < ApplicationRecord
 	belongs_to :user, optional: true
 	validates :username, :tonername, :quantity, :sectionname, :requestdate, :presence => true
-        attr_readonly :username, :tonername, :quantity, :sectionname, :requestdate, :stationname
+        # attr_readonly :username, :tonername, :quantity, :sectionname, :requestdate, :stationname
 end
