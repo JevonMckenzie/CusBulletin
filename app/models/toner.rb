cafec22139b8d082363dc50c90857ac705f6a49d
Jevon Mckenzie <jevonmckenzie.wesleycollege@gmail.com>
2020-01-16 15:58:30 -0600

    created method for username

--- app/models/toner.rb
+++ app/models/toner.rb
@@ -2,4 +2,11 @@ class Toner < ApplicationRecord
 	belongs_to :user, optional: true
 	validates :username, :tonername, :quantity, :sectionname, :requestdate, :presence => true
         # attr_readonly :username, :tonername, :quantity, :sectionname, :requestdate, :stationname
+
+
+        def requistionnumber
+           return self.requisitionnumber
+        end
+
+
 end
