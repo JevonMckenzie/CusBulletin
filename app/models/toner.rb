3d968aa439777cae08d7707b4265cb5ff6fbd2f0
Jevon Mckenzie <jevonmckenzie.wesleycollege@gmail.com>
2020-01-16 16:17:13 -0600

    edited Toner model to add back read only attribute on username bc it is mapped to current user

--- app/models/toner.rb
+++ app/models/toner.rb
@@ -1,7 +1,7 @@
 class Toner < ApplicationRecord
 	belongs_to :user, optional: true
 	validates :username, :tonername, :quantity, :sectionname, :requestdate, :presence => true
-        # attr_readonly :username, :tonername, :quantity, :sectionname, :requestdate, :stationname
+         attr_readonly :username,  :quantity, :sectionname, :requestdate, :stationname
 
 
         def requistionnumber
